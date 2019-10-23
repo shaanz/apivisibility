9ce46e6169944290db9e749a9463efffecf4970e
shaanz <shaanz@gmail.com>
2019-10-23 21:38:56 +0800

    Update gendata.sh

--- gendata.sh
+++ gendata.sh
@@ -1,9 +1,11 @@
+server="http://22.76.11.12"
 while true;
  do
-   curl -X POST http://52.76.123.216/api/nf1;
-   curl -X GET http://52.76.123.216/api/nf1;
-   curl -X OPTIONS http://52.76.123.216/api/nf1;
-   curl -X POST http://52.76.123.216/api/errorfunction
-   curl -X GET http://52.76.123.216/
-   curl -X OPTIONS http://52.76.123.216/list
+   curl -X POST $server/api/nf1
+   curl -X GET $server/api/nf1;
+   curl -X OPTIONS $server/api/nf1;
+   curl -X POST $server/api/errorfunction
+   curl -X GET $server
+   curl -X OPTIONS $server/list
  done
+~       
